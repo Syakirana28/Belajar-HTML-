1f80770b8824ab0e75958070f932996587bc83c4
Syakirana28 <110509134+Syakirana28@users.noreply.github.com>
2022-10-10 20:34:42 +0700

    Add files via upload

---
+++ PHP-SMK-video-25/restoran/kategori/update.php
@@ -0,0 +1,40 @@
+update data
+
+<?php 
+
+    require_once "../function.php";
+
+    $sql = "SELECT * FROM tblkategori WHERE idkategori = $id";
+    $result = mysqli_query($koneksi,$sql);
+    $row = mysqli_fetch_assoc($result);
+
+    // $kategori = 'jelly bean';
+    // $id = 14;
+    // $sql = "UPDATE tblkategori SET kategori='$kategori' WHERE idkategori= $id";
+
+    // $result = mysqli_query($koneksi, $sql);
+
+    // echo $sql;
+
+?>
+
+<form action="" method="post">
+
+    Kategori:
+    <input type="text" name="kategori" value="<?php echo $row['kategori']?>">
+    <br>
+    <input type="submit" name="simpan" value="simpan">
+
+</form>
+
+<?php 
+
+    if (isset($_POST['simpan'])) {
+        $kategori = $_POST['kategori'];
+        $sql = "UPDATE tblkategori SET kategori='$kategori' WHERE idkategori=$id";
+        $result = mysqli_query($koneksi, $sql);
+
+        header ("http://localhost/PHP-SMK/PHP-SMK-video-22/restoran/kategori/select.php");
+    }
+
+?>
